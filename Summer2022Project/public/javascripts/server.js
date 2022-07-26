09d51071560fd2346221e9afeea17b604b79ac39
vikas-vallabhaneni <vikasv2002@gmail.com>
2022-07-25 20:36:51 -0400

    updated comments

--- Summer2022Project/public/javascripts/server.js
+++ Summer2022Project/public/javascripts/server.js
@@ -26,45 +26,61 @@ server.get("/", function(req, res){
     res.render("index.ejs");
 })
 
-
-
-// add a page not found
+//  ************** add a page not found
 
 // on a post request, take the information and add it to the file
 server.post("/addAssignment", function(req, res){
     addBlockToFile(req.body);
+    // send you back to the home page
     res.redirect("/");
 })
 
 // deletes information for a user
 server.post("/deleteAssignment", function(req, res){
     deleteBlockFromFile(req.body);
+    // send you back to the home page
     res.redirect("/");
 })
 
+// has the application listen on the port provided
 server.listen(port, function(error){
+    // if there was an error, display it to console
     if (error){
         console.log('Something went wrong',  error)
     }
+    // else, log what port the server is listening on
     else {
         console.log('Server is listening on port ' + port);
     }
 });
 
-
+// this function adds an assignment block to the file storing all the assignment
+// block information
+//
+// Preconditions: formInput is either empty or valid data
+//
+// Postconditions: data.txt is updated with the information provided with the param formInput
+//
+// @param formInput
+//        the body of the POST request
+// 
 function addBlockToFile(formInput){
     // checks if input is empty
     if(JSON.stringify(formInput) === '{}')
         return;
+    // store the date
     let date = new Date(convertingDateFormat(formInput.dueDate));
+    // store the key
     let key = (date.getMonth()+1).toString() + "/" + date.getDate().toString() + "/" + date.getFullYear().toString();
+    // for every assignment the user has
     for (let i = 0; i<assignmentEntries.length; i++){
-        // if there is a key
+        // check if the key of the new assignment matches the key of a prexisting block
         if (assignmentEntries[i].includes(key + "\`")){
             // added to existing key
             assignmentEntries[i] = assignmentEntries[i].concat(";" + formInput.assignName + "|" + formInput.subjectColor + "|" + formInput.dueTime);
-            // add back to file
+            // set data to all the entries
             let data = assignmentEntries.filter(Boolean).join("\r\n") + "\r\n";
+            // add back to file
             fs.writeFile(pathToDataFile, data, (err) => {
                 if (err) console.log(err);
             });
@@ -72,56 +88,77 @@ function addBlockToFile(formInput){
             return;
         }
     }
-    // if no other key there
+    // if no other assignment has the same key
     let data = key + "\`" + formInput.assignName + "|" + formInput.subjectColor + "|" + formInput.dueTime;
+    // add the new assignment to the assignmentsEntries arr
     assignmentEntries.push(data);
+    // append it to the file
     fs.appendFile(pathToDataFile, data + "\r\n", (err) => {
         if (err) console.log(err);
     });
     
 }
 
+// this function deletes an assignment block from the file
+//
+// Preconditions: formInput is either empty or valid data
+//
+// Postconditions: data.txt is updated by removing the data provided by the param formInput
+//
+// @param formInput
+//        the body of the POST request
+// 
 function deleteBlockFromFile(formInput){
     // checks if input is empty
     if(JSON.stringify(formInput) === '{}')
         return;
+    // holds the date
     let date = new Date(formInput.dueDate);
+    // stores the key
     let key = (date.getMonth()+1).toString() + "/" + date.getDate().toString() + "/" + date.getFullYear().toString();
+    // for every assignment the user has
     for (let i = 0; i<assignmentEntries.length; i++){
-        // see if entry matches the key
+        // check if the key of the new assignment matches the key of a prexisting block
         if (assignmentEntries[i].includes(key + "\`")){
-            // see if there is only one assignment
+            // holds all the assignments for that key (day)
             let assignments = assignmentEntries[i].split('`')[1].split(";");
+            // if there is only
             if (assignments.length == 1){
+                // remove that entry
                 assignmentEntries[i] = "";
             }
-            // there is more than one assignment on that day
+            // else, if there is more than one assignment on that day
             else{
                 // for every assignment due that day
                 for (let j = 0; j<assignments.length; j++){
                     // if that assignment has the matching name and time and color of the assignment to delete
                     if (assignments[j].includes(formInput.assignName) && assignments[j].includes(formInput.dueTime) && assignments[j].toLowerCase().includes(formInput.subjectColor)){
-                        console.log(assignments[j])
+                        // delete that assignment
                         assignments[j] = "";
                     }
-                    let newAssignments = new Array();
-                    // for each assignment
-                    for (assignment of assignments){
-                        // if the assignment wasn't set to empty
-                        if (assignment != "")
-                            // add that assignment back
-                            newAssignments.push(assignment);
-                    }
-                    let tempArr = assignmentEntries[i].split('`');
-                    tempArr[1] = newAssignments.join(";");
-                    assignmentEntries[i] = tempArr.join('`');
-
                 }
+                // an array to hold the new list of assignments for that day
+                let newAssignments = new Array();
+                // for each assignment
+                for (assignment of assignments){
+                    // if the assignment wasn't set to empty
+                    if (assignment != "")
+                        // add that assignment back
+                        newAssignments.push(assignment);
+                }
+                // temp arr hold the line of data for that key (day), split on the key
+                let tempArr = assignmentEntries[i].split('`');
+                // tempArr[0] = key, tempArr[1] = assignment data      (thus we want to join on the post-deletion data)
+                tempArr[1] = newAssignments.join(";");
+                // set that entry to be the key with the updated post-deletion data
+                assignmentEntries[i] = tempArr.join('`');
             }
-            // add back to file
+            // data holds updated data
             let data = assignmentEntries.filter(Boolean).join("\r\n");
+            // makes sure if data file is blank, it doesnt add another new line
             if (data.length>0)
                 data += "\r\n";
+            // updates file with new data
             fs.writeFile(pathToDataFile, data, (err) => {
                 if (err) console.log(err);
             });
